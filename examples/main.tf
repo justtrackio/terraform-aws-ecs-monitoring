1f472692c569742d0735bf4c4d90806a1d9ccb88
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-01-25 07:54:47 +0100

    feat: create module

---
+++ examples/main.tf
@@ -0,0 +1,17 @@
+data "aws_vpcs" "default" {
+  tags = {
+    Name = "example"
+  }
+}
+
+data "aws_ecs_cluster" "default" {
+  cluster_name = "example"
+}
+
+module "ecs_monitoring" {
+  source          = "../"
+  stage           = "ecs"
+  name            = "monitoring"
+  ecs_cluster_arn = data.aws_ecs_cluster.default.arn
+  vpc_id          = data.aws_vpcs.default.id
+}
