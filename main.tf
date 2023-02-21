62596e7b67ce4ca162a8205da27af28d34c6ed91
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-02-21 10:29:36 +0100

    feat: create module (#1)

---
+++ main.tf
@@ -0,0 +1,198 @@
+module "cadvisor_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  attributes  = ["cadvisor"]
+  label_order = var.label_orders.cloudwatch
+
+  context = module.this.context
+}
+
+module "node_exporter_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  attributes  = ["node-exporter"]
+  label_order = var.label_orders.cloudwatch
+
+  context = module.this.context
+}
+
+resource "aws_cloudwatch_log_group" "node_exporter" {
+  name              = module.node_exporter_label.id
+  tags              = module.node_exporter_label.tags
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_group" "cadvisor" {
+  name              = module.cadvisor_label.id
+  tags              = module.cadvisor_label.tags
+  retention_in_days = var.log_retention_in_days
+}
+
+module "node_exporter_definition" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  interactive              = false
+  readonly_root_filesystem = true
+  container_name           = "node-exporter"
+  container_image          = "quay.io/prometheus/node-exporter:${var.node_exporter_version}"
+  mount_points = [
+    {
+      containerPath = "/host"
+      sourceVolume  = "root"
+      readOnly      = true
+    }
+  ]
+  command = [
+    "--path.rootfs=/host"
+  ]
+  container_memory_reservation = var.container_memory_reservation
+  container_cpu                = var.node_exporter_cpu
+  map_environment              = var.environment_variables
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = aws_cloudwatch_log_group.node_exporter.name
+      awslogs-region        = var.aws_region
+      awslogs-stream-prefix = "ecs"
+    }
+  }
+}
+
+module "cadvisor_definition" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  interactive              = false
+  readonly_root_filesystem = true
+  container_name           = "cadvisor"
+  container_image          = "gcr.io/cadvisor/cadvisor:${var.cadvisor_version}"
+  mount_points = [
+    {
+      containerPath = "/sys/fs/cgroup"
+      sourceVolume  = "cgroup"
+      readOnly      = true
+    },
+    {
+      containerPath = "/rootfs"
+      sourceVolume  = "root"
+      readOnly      = true
+    },
+    {
+      containerPath = "/var/run"
+      sourceVolume  = "var_run"
+      readOnly      = false
+    },
+    {
+      containerPath = "/sys"
+      sourceVolume  = "sys"
+      readOnly      = true
+    },
+    {
+      containerPath = "/var/lib/docker"
+      sourceVolume  = "var_lib_docker"
+      readOnly      = true
+    },
+    {
+      containerPath = "/dev/disk"
+      sourceVolume  = "dev_disk"
+      readOnly      = true
+    }
+  ]
+  privileged = true
+  linux_parameters = {
+    capabilities = {
+      add  = null
+      drop = null
+    }
+    devices = [
+      {
+        containerPath = null
+        hostPath      = "/dev/kmsg"
+        permissions   = ["read"]
+      }
+    ]
+    initProcessEnabled = null
+    maxSwap            = null
+    sharedMemorySize   = null
+    swappiness         = null
+    tmpfs              = []
+  }
+  command = [
+    "--port=8001",
+    "--store_container_labels=false",
+    "--enable_load_reader=true",
+    "--whitelisted_container_labels=com.amazonaws.ecs.cluster,com.amazonaws.ecs.task-definition-family,com.amazonaws.ecs.container-name,com.amazonaws.ecs.task-definition-version",
+    "--docker_only=true",
+    "--disable_metrics=advtcp,cpuset,cpu_topology,disk,hugetlb,memory_numa,process,referenced_memory,resctrl,sched,tcp,udp",
+    "--housekeeping_interval=2s"
+  ]
+  container_cpu                = var.cadvisor_cpu
+  container_memory_reservation = var.container_memory_reservation
+  map_environment = merge(
+    var.environment_variables,
+    {
+      CADVISOR_HEALTHCHECK_URL = "http://localhost:8001/healthz"
+    }
+  )
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = aws_cloudwatch_log_group.cadvisor.name
+      awslogs-region        = var.aws_region
+      awslogs-stream-prefix = "ecs"
+    }
+  }
+}
+
+module "ecs_service_task" {
+  source  = "justtrackio/ecs-alb-service-task/aws"
+  version = "1.0.0"
+
+  container_definition_json = "[${module.node_exporter_definition.json_map_encoded},${module.cadvisor_definition.json_map_encoded}]"
+  ecs_cluster_arn           = var.ecs_cluster_arn
+  launch_type               = "EC2"
+  scheduling_strategy       = "DAEMON"
+  task_cpu                  = var.task_cpu
+  task_memory               = var.task_memory
+  network_mode              = "host"
+  docker_volumes = [
+    {
+      host_path                   = "/"
+      name                        = "root"
+      docker_volume_configuration = []
+    },
+    {
+      host_path                   = "/var/run"
+      name                        = "var_run"
+      docker_volume_configuration = []
+    },
+    {
+      host_path                   = "/sys"
+      name                        = "sys"
+      docker_volume_configuration = []
+    },
+    {
+      host_path                   = "/var/lib/docker"
+      name                        = "var_lib_docker"
+      docker_volume_configuration = []
+    },
+    {
+      host_path                   = "/dev/disk"
+      name                        = "dev_disk"
+      docker_volume_configuration = []
+    },
+    {
+      host_path                   = "/sys/fs/cgroup"
+      name                        = "cgroup"
+      docker_volume_configuration = []
+    }
+  ]
+  vpc_id         = var.vpc_id
+  propagate_tags = "SERVICE"
+
+  label_orders = var.label_orders
+  context      = module.this.context
+}
---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+  default     = null
+}
+
+variable "node_exporter_version" {
+  type        = string
+  description = "Node exporter version to be deployed"
+  default     = "v1.5.0"
+}
+
+variable "cadvisor_version" {
+  type        = string
+  description = "cAdvisor version to be deployed"
+  default     = "v0.47.0"
+}
+
+variable "container_memory_reservation" {
+  type        = number
+  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
+  default     = 128
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  description = "The environment variables to pass to the container. This is a map of string: {key: value}. map_environment overrides environment"
+  default     = null
+}
+
+variable "node_exporter_cpu" {
+  type        = number
+  description = "Number of CPU units to reserve for the container"
+  default     = 100
+}
+
+variable "cadvisor_cpu" {
+  type        = number
+  description = "Number of CPU units to reserve for the container"
+  default     = 100
+}
+
+variable "log_retention_in_days" {
+  type        = number
+  description = "Specifies the number of days you want to retain log events in the specified log group."
+  default     = 1
+}
+
+variable "task_cpu" {
+  type        = number
+  description = "Number of CPU units to reserve for the container"
+  default     = 256
+}
+
+variable "task_memory" {
+  type        = number
+  description = "The amount of memory (in MiB) used by the task. If using Fargate launch type `task_memory` must match [supported cpu value](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
+  default     = 512
+}
+
+variable "ecs_cluster_arn" {
+  type        = string
+  description = "ECS cluster ARN where this service will be deployed"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where resources are created"
+}
+
+variable "label_orders" {
+  type = object({
+    cloudwatch = optional(list(string)),
+    ecs        = optional(list(string)),
+    iam        = optional(list(string)),
+    vpc        = optional(list(string))
+  })
+  default     = {}
+  description = "Overrides the `labels_order` for the different labels to modify ID elements appear in the `id`"
+}
